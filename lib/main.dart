4ab49e238d2f846d92baf2c9e5c3013f61b9a6ef
YhomiAce <kareem.yomi@scelloo.com>
2022-01-03 22:19:10 +0100

    Applifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   final List<Transaction> _userTransactions = [
     // Transaction(
     //     id: 't1', title: 'New Shoes', amount: 57.89, date: DateTime.now()),
@@ -139,6 +139,23 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       txListWidget
     ];
+  }
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print(state);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
   }
 
   @override
--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key key,
     @required this.transaction,
@@ -11,6 +13,24 @@ class TransactionItem extends StatelessWidget {
 
   final Transaction transaction;
   final Function deleteTx;
+
+  @override
+  _TransactionItemState createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  Color _bgColor;
+  @override
+  void initState() {
+    super.initState();
+    const availableColors = [
+      Colors.red,
+      Colors.black,
+      Colors.blue,
+      Colors.purple,
+    ];
+    _bgColor = availableColors[Random().nextInt(4)];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,19 +43,19 @@ class TransactionItem extends StatelessWidget {
       elevation: 5,
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bgColor,
           radius: 30,
           child: Padding(
             padding: const EdgeInsets.all(6.0),
-            child: FittedBox(
-                child: Text("\$${transaction.amount}")),
+            child: FittedBox(child: Text("\$${widget.transaction.amount}")),
           ),
         ),
         title: Text(
-          transaction.title,
+          widget.transaction.title,
           style: themeStyle.textTheme.title,
         ),
         subtitle: Text(
-          DateFormat.yMMMd().format(transaction.date),
+          DateFormat.yMMMd().format(widget.transaction.date),
         ),
         trailing: MediaQuery.of(context).size.width > 460
             ? FlatButton.icon(
@@ -43,14 +63,14 @@ class TransactionItem extends StatelessWidget {
                 icon: const Icon(Icons.delete),
                 label: const Text("Delete"),
                 onPressed: () {
-                  deleteTx(transaction.id);
+                  widget.deleteTx(widget.transaction.id);
                 },
               )
             : IconButton(
                 icon: const Icon(Icons.delete),
                 color: themeStyle.errorColor,
                 onPressed: () {
-                  deleteTx(transaction.id);
+                  widget.deleteTx(widget.transaction.id);
                 },
               ),
       ),
